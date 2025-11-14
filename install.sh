7a16524de00e649165eab4eea61a5641217095d1
lyqray <lyqray@outlook.com>
2025-11-14 11:05:15 -0500

    修改脚本地址、compose、build

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 下载地址
-DOWNLOAD_URL="https://github.com/bqlpfy/flux-panel/releases/download/1.4.2/gost"
+DOWNLOAD_URL="https://github.com/lyqray/flux-panel/releases/download/1.4.2/gost"
 INSTALL_DIR="/etc/gost"
 COUNTRY=$(curl -s https://ipinfo.io/country)
 if [ "$COUNTRY" = "CN" ]; then
